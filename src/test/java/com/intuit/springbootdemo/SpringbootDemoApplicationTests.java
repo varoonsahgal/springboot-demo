bef262547fba0e6ccb49c6efd5f31ac1da2d2f22
varoonsahgal <varoon.sahgal@gmail.com>
2025-12-11 17:42:17 -0500

    adding idea

--- src/test/java/com/intuit/springbootdemo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/intuit/springbootdemo/SpringbootDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
